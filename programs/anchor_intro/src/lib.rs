2670f258fc18f20b9f9787f5a45812bcd01489cf
Niferu <wargacki.patryq@gmail.com>
2024-02-27 15:22:07 +0100

    todo removed

--- programs/anchor_intro/src/lib.rs
+++ programs/anchor_intro/src/lib.rs
@@ -2,19 +2,6 @@ use anchor_lang::prelude::*;
 
 declare_id!("BHcCV7N2w6bMsugoE5URT3YM52PVnezjcUkDNz8VT7no");
 
-/** @TODO:
-    ● Escrow contracts should maintain separate accounts for each user, allowing efficient handling of multiple users simultaneously
-    ● Escrow contracts allow for the multi-call of entrypoints: deposit and withdraw.
-    ● Only the owner of an account should be able to withdraw funds from their respective escrow account.
-
-    ● Utilize a generic token type (SPL Token, Erc20, PSP22) based on your chosen ecosystem.
-    ● Address potential security concerns, particularly vulnerabilities like reentrancy.
-    ● Include unit tests.
-    ● Bonus points if you add end-to-end tests in TypeScript.
-    ● Bonus points if you implement deposit and withdrawal in a single transaction (swap). The swap may have different levels of complexity, e.g., always 1 to 1, AMM, order book,
-      depending on your skills.
- */
-
 #[program]
 pub mod anchor_intro {
     use super::*;
